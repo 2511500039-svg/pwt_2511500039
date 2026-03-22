13dd287bc48aa6e293aa5bccca1cc5512b041003
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-03-23 02:21:57 +0700

    wow

--- koneksi.php
+++ koneksi.php
@@ -1,14 +1,12 @@
 <?php
-// Konfigurasi database
-$db_host = "localhost"; // host database
-$db_user = "root";      // username database
-$db_pass = "";          // password database
-$db_name = "jadwalguru"; // nama database
 
-// Membuat koneksi
+$db_host = "localhost"; 
+$db_user = "root";      
+$db_pass = "";          
+$db_name = "jadwal"; 
+
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-// Cek koneksi
 if (!$koneksi) {
     die("Gagal melakukan koneksi ke database: " . mysqli_connect_error());
 }
