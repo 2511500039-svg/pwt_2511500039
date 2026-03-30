98426a6d00f7194a38eea86d17f5cf04b60248b1
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-03-30 10:37:47 +0700

    WowWow

--- login.php
+++ login.php
@@ -8,93 +8,106 @@ session_start();
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>AdminLTE 3 | Log in</title>
 
+<!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<!-- Font Awesome -->
 <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+<!-- iCheck bootstrap -->
 <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+<!-- Theme style -->
 <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 
 <body class="hold-transition login-page">
+
 <div class="login-box">
 
 <div class="login-logo">
 <a href="#"><b>Admin</b>LTE</a>
 </div>
+<!-- /.login-logo -->
 
 <div class="card">
+
 <div class="card-body login-card-body">
+
 <p class="login-box-msg">Sign in to start your session</p>
 
-<form action="" method="post">
+<form action="#" method="post">
+  <div class="input-group mb-3">
+    <input type="text" name="username" id="username" class="form-control" placeholder="username">
+    <div class="input-group-append">
+      <div class="input-group-text">
+        <span class="fas fa-envelope"></span>
+      </div>
+    </div>
+  </div>
 
-<div class="input-group mb-3">
-<input type="text" name="Username" id="Username" class="form-control" placeholder="Username" required>
-<div class="input-group-append">
-<div class="input-group-text">
-<span class="fas fa-envelope"></span>
-</div>
-</div>
-</div>
+  <div class="input-group mb-3">
+    <input type="password" name="password" id="password" class="form-control" placeholder="password">
+    <div class="input-group-append">
+      <div class="input-group-text">
+        <span class="fas fa-lock"></span>
+      </div>
+    </div>
+  </div>
 
-<div class="input-group mb-3">
-<input type="password" name="Password" id="Password" class="form-control" placeholder="Password" required>
-<div class="input-group-append">
-<div class="input-group-text">
-<span class="fas fa-lock"></span>
-</div>
-</div>
-</div>
-
-<div class="row">
-<div class="col-12">
-<input type="submit" name="login" value="Login" class="btn btn-primary btn-block">
-</div>
-</div>
-
+  <div class="row">
+    <!-- /.col -->
+    <div class="col-12">
+      <input type="submit" name="login" value="Login" class="btn btn-primary btn-block">
+    </div>
+    <!-- /.col -->
+  </div>
 </form>
 
-<?php
-if(isset($_POST['Username']) && isset($_POST['Password'])) {
-    $Username = $_POST['Username'];
-    $Password = $_POST['Password'];
-
-    if(empty($Username) || empty($Password)) {
-        echo '<div class="alert alert-warning">Data Tidak Boleh kosong</div>';
-    } else {
-        // Query login
-        $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$Username' AND password='$Password' AND role='admin'");
-        
-        if($query) {
-            $user = mysqli_fetch_array($query);
-            if($user) {
-                $_SESSION['level'] = $user['role'];
-                $_SESSION['Username'] = $user['username'];
-                header("Location: index.php");
-                exit();
-            } else {
-                echo '<div class="alert alert-danger alert-dismissible">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <h5><i class="icon fas fa-ban"></i> Alert!</h5>
-                Username atau Password salah
-                </div>';
-            }
-        } else {
-            echo "Query gagal: " . mysqli_error($koneksi);
-        }
-    }
-}
-?>
+</div>
+<!-- /.login-card-body -->
 
 </div>
 </div>
+<!-- /.login-box -->
 
-</div>
-
+<!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap 4 -->
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
 
 </body>
 </html>
+
+<?php
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if (empty($username) || empty($password)) {
+        echo "Data Tidak Boleh kosong";
+    } else {
+        $userquery = mysqli_fetch_array(mysqli_query($koneksi, 
+        "SELECT * FROM users WHERE username = '$username' AND password = '$password'"));
+
+        if ($userquery) {
+            $_SESSION['role'] = $userquery['role'];
+            $_SESSION['username'] = $username;
+            header("location:index.php");
+          } else {
+            echo '<div class="alert alert-danger alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+            <h5><i class="icon fas fa-ban"></i> Alert!</h5>
+            Login gagal
+            </div>';
+        }
+      }
+    }
+?>
