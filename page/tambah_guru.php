378aaae134b0cff7ad63db692bd18c5c70a25184
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-04-14 12:23:59 +0700

    wowowowo.

---
+++ page/tambah_guru.php
@@ -0,0 +1,91 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Data Guru</h1>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+//kode ootmtis
+$carikode = mysqli_query($koneksi,"select max(kd_guru) from guru") or die ( mysqli_error($koneksi));
+$datakode = mysqli_fetch_array($carikode);
+if($datakode) {
+    $nilaikode = substr($datakode[0], 2);
+    $kode = (int) $nilaikode;
+    $kode = $kode + 1;
+    $hasilkode ="M-".str_pad($kode, 3, "0", STR_PAD_LEFT);
+} else { $hasilkode ="M-"; }
+$_SESSION['KODE'] = $hasilkode;
+
+if(isset($_POST['tambah'])){
+    $kd_guru = $_POST['kd_guru'];
+    $id_users = $_POST['id_users'];
+    $nm_guru = $_POST['nm_guru'];
+    $jenkel = $_POST['Jenkel'];
+    $pend_terakhir = $_POST['Pend_terakhir'];
+    $hp = $_POST['HP'];
+    $alamat = $_POST['Alamat'];
+
+    $insert = mysqli_query($koneksi,"INSERT INTO guru values ('$kd_guru','$id_users','$nm_guru','$jenkel','$pend_terakhir','$hp','$alamat')");
+    if ($insert) {
+        echo "<div class='alert alert-info-dismissible'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>x</button>
+        <h5><i class='icon fas fa-info'></i> Info </h5>
+        <h4>Berhasil Disimpan</h4></div>";
+        echo "<meta http-equiv='refresh' content='1;url=index.php?page=guru'>";
+    }else{
+        echo "<div class='alert alert-warning alert-dismissible'>
+        <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>x</button>
+        <h5><i class='icon fas fa-info'></i> Info </h5>
+        <h4>Gagal Disimpan</h4></div>";
+    }
+}
+?>
+<section class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <div class="card-body p-2">
+                    <form method="POST" action="">
+                        <div class="form-group">
+                            <label for="kd_guru">Kd Guru</label>
+                            <input type="text" name="kd_guru" id="kd_guru" placeholder="Id Guru" class="form-control" value="<?php echo $_SESSION['KODE']; ?>" readonly>
+                        </div>
+                        <div class="form-group">
+                            <label for="id_users">Id Users</label>
+                            <input type="text" name="id_users" id="id_users" placeholder="Id Users" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="nm_guru">Nama Guru</label>
+                            <input type="text" name="nm_guru" id="nm_guru" placeholder="Nama Guru" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="jenkel">Jenis Kelamin</label>
+                            <select name="jenkel" id="jenkel" class="form-control">
+                                <option value="Laki-laki">Laki-laki</option>
+                                <option value="Perempuan">Perempuan</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="pend_terakhir">Pendidikan Terakhir</label>
+                            <input type="text" name="pend_terakhir" id="pend_terakhir" placeholder="Pendidikan Terakhir" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="hp">No HP</label>
+                            <input type="text" name="hp" id="hp" placeholder="No HP" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="alamat">Alamat</label>
+                            <textarea name="alamat" id="alamat" placeholder="Alamat" class="form-control"></textarea>
+                        </div>
+                        <div class="card-footer">
+                            <input type="submit" class="btn btn-primary" name="tambah" value="Simpan">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
