d8a6ee8fcfcc6d4c481487a7e13a1b7fe33b4afb
unknown <2511500039@mahasiswa.atmaluhur.ac.id>
2026-05-05 00:05:57 +0700

    wowowowo.

---
+++ page/guru.php
@@ -0,0 +1,79 @@
+<div class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1 class="m-0 text-dark">Data Guru</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == "hapus") {
+        $kd = $_GET['kd'];
+        $query = mysqli_query($koneksi, "DELETE FROM guru WHERE id_guru ='$kd'");
+
+        if ($query) {
+            echo "<div class='alert alert-warning alert-dismissible'>
+                    Berhasil Di Hapus
+                  </div>";
+            echo "<meta http-equiv='refresh' content='1;url=index.php?page=guru'>";
+        } else {
+            die("Query Error (DELETE): " . mysqli_error($koneksi));
+        }
+    }
+}
+?>
+
+<div class="content">
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-body">
+                <a href="index.php?page=tambah_guru" class="btn btn-primary btn-sm">Tambah Guru</a>
+
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>ID Guru</th>
+                            <th>Nama Guru</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+
+                    <?php
+                    $no = 0;
+                    $query = mysqli_query($koneksi, "SELECT * FROM guru");
+
+                    if (!$query) {
+                        die("Query Error (SELECT): " . mysqli_error($koneksi));
+                    }
+
+                    while ($result = mysqli_fetch_array($query)) {
+                        $no++;
+                    ?>
+
+                    <tbody>
+                        <tr>
+                            <td><?php echo $no; ?></td>
+                            <td><?php echo $result['id_guru']; ?></td>
+                            <td><?php echo $result['nm_guru']; ?></td>
+                            <td>
+                                <a href="index.php?page=edit_guru&kd=<?= $result['id_guru']; ?>">
+                                    <span class="badge badge-warning">Edit</span>
+                                </a>
+
+                                <a href="index.php?page=guru&action=hapus&kd=<?= $result['id_guru']; ?>">
+                                    <span class="badge badge-danger">Hapus</span>
+                                </a>
+                            </td>
+                        </tr>
+                    </tbody>
+
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
